3228d692b6b2a4480cec6a0c590c5618901f7114
藍月暗い <Kurai32761@gmail.com>
2024-11-16 05:14:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -38,10 +38,10 @@ module.exports = {
 
 nodes: [
     {
-        name: "Working1",
-        "host": "lava.catfein.com",
-        "port": 4000,
-        "password": "catfein",
+        name: "Working2",
+        host: "lavalink.clxud.dev",
+        port: 2333,
+        password: "youshallnotpass",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
