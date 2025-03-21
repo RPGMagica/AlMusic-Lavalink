16e4376702225cdb7a0d121bea4eac433ed8eb79
RPGMagica <143371962+RPGMagica@users.noreply.github.com>
2025-03-22 08:05:32 +1000

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "Sick Beats", 
+  activityType: "Playing",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
