8390b49f6453754304be50141c6ecce318bd22bc
RPGMagica <143371962+RPGMagica@users.noreply.github.com>
2025-03-26 10:17:29 +1000

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,13 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
-      secure: false
+      name: "Node #1",
+        host: "lavalinkv3-id.serenetia.com",
+        port: 443,
+        password: "BatuManaBisar",
+        reconnectTimeout: 5000,
+        reconnectTries: Infinity,
+        secure: true
     }
   ]
 }
